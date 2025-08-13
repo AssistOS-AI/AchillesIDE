3306a5c9194c994bca5dce495ef40c01e1db7cdf
alupoaiemircea <mircea@axiologic.net>
2025-08-13 11:45:41 +0300

    added structure

---
+++ web-components/achilles-ide-landing/achilles-ide-landing.js
@@ -0,0 +1,18 @@
+export class AchillesIdeLanding {
+    constructor(element, invalidate) {
+        this.element = element;
+        this.invalidate = invalidate;
+        this.invalidate();
+    }
+
+    async beforeRender() {
+
+  
+    }
+
+
+    async afterRender() {
+
+    }
+}
+  
